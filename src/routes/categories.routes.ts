bb20d097063d1b3e7621911bd44494638155abb6
diego.chiodini <diego.chiodini@esmenezes.com.br>
2024-03-21 10:10:04 -0300

    refactor: Controller, Repositories e Routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,19 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
+import { CreateCategoryUseCase } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-    createCategoryService.execute({ name, description})
-
-    return response.status(201).send();
+   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', ( request, response) => {
-    const all = categoriesRepository.list();
-
-    return response.json(all);
+   return listCategoriesController.handle(request, response);
 })
 
 export { categoriesRoutes };
